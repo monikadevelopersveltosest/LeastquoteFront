ea2aa441245d044bcccd9b45e17eab63fcbe5358
rohit0035 <rohitdevelopersveltosest@gmail.com>
2021-08-25 13:11:15 +0530

    bugs solve in php

--- application/controllers/Adminnew.php
+++ application/controllers/Adminnew.php
@@ -554,12 +554,6 @@ class Adminnew extends CI_Controller {
 
 
     	//$url = base_url()."adminnew/productlist";
-<<<<<<< HEAD
-
-		//$data["links"] = $this->pagination($url,$data['pagination'],$this->input->get('per_page'),total_per_page);
-		$data['rows'] = $this->Common_model->getwhere('dummyproduct',array());
-=======
->>>>>>> e223e2f5bc562509f7bfe4a05af9e543d49ab907
 
 		//$data["links"] = $this->pagination($url,$data['pagination'],$this->input->get('per_page'),total_per_page);
 		$data['rows'] = $this->Common_model->getwhere('dummyproduct',array());
@@ -594,12 +588,8 @@ class Adminnew extends CI_Controller {
 		}			
 
 	}
-<<<<<<< HEAD
-	public function addproduct(){
-=======
 	public function addproduct($id=false){
 		//echo $id;die;
->>>>>>> e223e2f5bc562509f7bfe4a05af9e543d49ab907
 		$data = array();
 		$data['success'] = "";
 		$data['error'] = "";
@@ -607,7 +597,6 @@ class Adminnew extends CI_Controller {
 		if(isset($_POST['submit']))
 		{
 			$user_data = array();
-<<<<<<< HEAD
 
 			$filearray = array();
 			if (isset($_FILES)) {
@@ -628,28 +617,6 @@ class Adminnew extends CI_Controller {
 				$user_data['category_image'] = $filearray['category_image'];
 			}
 
-=======
-
-			$filearray = array();
-			if (isset($_FILES)) {
-			    //echo '<pre>';print_r($_FILES);die();
-			    foreach ($_FILES as $key => $value){
-			        //print_r($value['size']);
-			        if($value['size'] > 0) {
-						$filearraydata = $this->uploadcategoryfile($key);
-			            $filearray[$key] = $filearraydata;
-			        }else{
-			            $this->session->set_flashdata('error_fileupload', 'File size is empty!');
-			        }
-			    }
-				//$post_data = $_POST+$filearray;
-			}
-
-			if(isset($filearray['category_image'])) {
-				$user_data['category_image'] = $filearray['category_image'];
-			}
-
->>>>>>> e223e2f5bc562509f7bfe4a05af9e543d49ab907
 			// print_r($filearray);
 			// die;
 			// $parent_id = $_POST['parent_id'];
@@ -678,11 +645,8 @@ class Adminnew extends CI_Controller {
 		if(!empty($id))
 		{
 		    $whr = array();
-<<<<<<< HEAD
-=======
 			$data['product_data'] = $this->Common_model->getSingleRecordById('dummyproduct', array("id"=>$id));
 
->>>>>>> e223e2f5bc562509f7bfe4a05af9e543d49ab907
 			$data['cat_data'] = $this->Common_model->getSingleRecordById('shopcategories', $whr);
 		}
 		$whr2 = array('1'=>1);
